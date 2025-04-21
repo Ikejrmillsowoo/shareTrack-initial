ef26221d301f4550539e95690317556d65217451
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-21 11:25:10 -0400

    starting bean creation

---
+++ src/main/java/com/zipcode/beans/Item.java
@@ -0,0 +1,51 @@
+package com.zipcode.beans;
+
+public class Item {
+
+    private Long id;
+    private String type;
+    private String model;
+    private String barCodeNumber;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBarCodeNumber() {
+        return barCodeNumber;
+    }
+
+    public void setBarCodeNumber(String barCodeNumber) {
+        this.barCodeNumber = barCodeNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
---
+++ src/main/java/com/zipcode/beans/Location.java
@@ -0,0 +1,73 @@
+package com.zipcode.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Location {
+
+    private Long id;
+    private String name;
+    private String addressStreet;
+    private String addressCity;
+    private String addressState;
+    private String addressZip;
+    private List<Item> inventory = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+    public void setAddressStreet(String addressStreet) {
+        this.addressStreet = addressStreet;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public String getAddressState() {
+        return addressState;
+    }
+
+    public void setAddressState(String addressState) {
+        this.addressState = addressState;
+    }
+
+    public String getAddressZip() {
+        return addressZip;
+    }
+
+    public void setAddressZip(String addressZip) {
+        this.addressZip = addressZip;
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Item> inventory) {
+        this.inventory = inventory;
+    }
+
+
+}
